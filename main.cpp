8b7a8e4aa903207d138873cb9c5945d6746abda2
Alagie Sellu <alagiesellu@gmail.com>
2021-10-23 10:06:05 +0000

    Added calculator grammar for ANTLR parser generator.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
+#include <stdint.h>
+#include <stdio.h>
+
 using namespace std;
 
-string scanner(string &input) {
+bool is_exit(const string& input) {
+    return input == "exit";
+}
 
-    cout << endl << "=> ";
+string input_scanner(string &input) {
+
+    cout << "=> ";
 
     getline(cin, input);
 
     return input;
 }
 
-bool is_exit(const string& input) {
-    return input == "exit";
+float hexadecimalToDecimal(const char *hexadecimal_string) {
+    uint32_t number;
+    sscanf(hexadecimal_string, "%x", &number);  // assuming you checked input
+    return *((float*)&number);
+}
+
+string input_parser(string &input) {
+
+    cout << "=> ";
+
+    getline(cin, input);
+
+    return input;
 }
 
 void calculator() {
     string input;
     while (! is_exit(input)) {
-        scanner(input);
+        input_scanner(input);
     }
 }
 
